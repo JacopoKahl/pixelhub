c1abe32905c12f84bddfc5ee19e900068df866ea
Jacopo Kahl <jacopokahl@gmail.com>
2020-12-12 14:23:46 +0100

    Changed urls structure

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
+from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+
+    name = models.TextField()
+
+    def __str__(self):
+        return self.name
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Blog
 
 # Create your views here.
+
+#https://www.udemy.com/course/python-django-tkinter-complete-bundle-advance/learn/lecture/16516338#overview
+
+def home(request):
+
+    return render(request, 'home.html')
